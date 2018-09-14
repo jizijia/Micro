28548f31c955cdf4c54e5bda1b2cd25fbeee7010
jizijia <jizijia@qq.com>
2018-09-14 14:22:39 +0800

    0

---
+++ Core/Micro.Web/Startup.cs
@@ -0,0 +1,85 @@
+﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Micro.Web
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        private readonly object loggerFactory;
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration; 
+            loggerFactory = NLog.LogManager.LoadConfiguration("nlog.config").GetCurrentClassLogger();
+        } 
+
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            // 跨域处理
+            services.AddCors(options =>
+            {
+                options.AddPolicy("any", builder =>
+                {
+                    builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials();
+                });
+            });
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
+                .AddControllersAsServices()
+                // Json 配置
+                .AddJsonOptions(x =>
+                {
+                    x.SerializerSettings.DateFormatString = "yyyy/MM/dd HH:mm:ss";
+                    x.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+                    x.SerializerSettings.NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore;
+                }); ;
+
+            // Swagger
+            services.AddSwaggerGen(options =>
+            {
+                options.DescribeAllEnumsAsStrings();
+                options.SwaggerDoc("v1", new Swashbuckle.AspNetCore.Swagger.Info
+                {
+                    Title = "测试项目 - Web API",
+                    Version = "v1"
+                });
+            });
+
+            var container = new ContainerBuilder();
+            container.Populate(services);
+            return new AutofacServiceProvider(container.Build());
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+
+
+            // Swagger
+            var pathBase = Configuration["PATH_BASE"];
+            if (!string.IsNullOrEmpty(pathBase))
+            {
+                app.UsePathBase(pathBase);
+            }
+            app.UseSwagger()
+               .UseSwaggerUI(c =>
+               {
+                   c.SwaggerEndpoint($"{ (!string.IsNullOrEmpty(pathBase) ? pathBase : string.Empty) }/swagger/v1/swagger.json", "RYH.API V1");
+               });
+        }
+    }
+}
